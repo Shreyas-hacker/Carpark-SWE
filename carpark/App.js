d196f16ce864354ae5f20a0ff50f6719a81df926
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-03-08 03:08:01 +0800

    Initial primary button

--- carpark/App.js
+++ carpark/App.js
@@ -1,16 +1,25 @@
 import { StyleSheet, Text, View, TextInput } from 'react-native';
-import ReportFault from './screens/ReportFault';
+import PrimaryButton from './components/PrimaryButton';
 
 export default function App() {
+
+  const success = true; // can change to false if want to see change in color of button
+
   return (
     <View style={styles.container}>
-      <ReportFault />
+      <View style={styles.buttonContainer}>
+        <PrimaryButton onSuccess={success}>Create</PrimaryButton>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+    backgroundColor: 'black',
+    flex: 1,
   },
+  buttonContainer:{
+    marginTop: 300,
+  }
 });
--- carpark/components/PrimaryButton.js
+++ carpark/components/PrimaryButton.js
@@ -1,11 +1,36 @@
+import { Children } from "react"
 import { Text, View, Pressable, StyleSheet } from "react-native"
 
-function PrimaryButton(){
+function PrimaryButton({children, onSuccess}){
     return (
-        <Text>This is a button!</Text>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable style={()=> onSuccess ? [styles.buttonInnerContainer, styles.success]: styles.buttonInnerContainer}>
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
     )
 }
 
 export default PrimaryButton
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    buttonOuterContainer:{
+        borderWidth: 1,
+        borderRadius: 28,
+        overflow: 'hidden',
+        width: 130,
+    },
+    buttonInnerContainer:{
+        backgroundColor: 'grey',
+        paddingVertical: 10,
+    },
+    buttonText:{
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    success:{
+        backgroundColor:'#39D2C0',
+    }
+})
