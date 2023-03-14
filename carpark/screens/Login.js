b244633f2b21f8eb6e34b36d043c54000224f676
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-03-15 01:34:00 +0800

    Updated Login page with some hooks

--- carpark/screens/Login.js
+++ carpark/screens/Login.js
@@ -1,15 +1,30 @@
-import { View, Text, StyleSheet } from "react-native";
-
+import { View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
 
 function Login(){
-    const success = true; // can change to false if want to see change in color of button
+    const [filled, setfilled] = useState(false); // state to manage if all fields in the form has been filled
+    const [username,setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
+    function passwordHandler(enteredPassword){
+        setPassword(enteredPassword);
+    }
+
+    function usernameHandler(enteredUsername){
+        setUsername(enteredUsername);
+    }
+
+    useEffect(()=>{
+        if(username!=='' && password!==''){
+            setfilled(true);
+        }
+    },[username,password])
     return(
         <View style={styles.form}>
             <View style={styles.container}>
                 <View style={styles.buttonContainer}>
-                    <PrimaryButton onSuccess={success}>Create</PrimaryButton>
+                    <PrimaryButton onSuccess={filled}>Create</PrimaryButton>
                 </View>
             </View>
         </View>
