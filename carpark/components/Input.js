60ec24781754b6d82676b93bff1c089514d19feb
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-03-16 14:35:04 +0800

    initial login layout without styling for text input

--- carpark/components/Input.js
+++ carpark/components/Input.js
@@ -1,11 +1,39 @@
 import { StyleSheet, View, Text, TextInput } from "react-native";
 
-function Input(){
+function Input({ label, textInputConfig }) {
+    let inputStyles = [styles.text];
 
+    if (textInputConfig && textInputConfig.multiline){
+        inputStyles.push(styles.inputMultiline);
+    }
+
+    return (
+        <View style={styles.inputContainer}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput style={inputStyles} {...textInputConfig}/>
+        </View>
+    );
 }
 
 export default Input;
 
 const styles = StyleSheet.create({
+    inputContainer:{
+        marginHorizontal: 4,
+        marginVertical: 8
+    },
+    label:{
+        fontSize:12,
+        marginBottom: 4,
+    },
+    text:{
+        padding: 6,
+        borderRadius: 12,
+        fontSize: 18,
+    },
+    inputMultiline:{
+        minHeight: 100,
+        textAlignVertical: "top"
+    }
 
-})
+});
--- carpark/screens/Login.js
+++ carpark/screens/Login.js
@@ -1,6 +1,7 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, Dimensions, Pressable } from "react-native";
 import { useEffect, useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
+import Input from "../components/Input";
 
 function Login(){
     const [filled, setfilled] = useState(false); // state to manage if all fields in the form has been filled
@@ -20,28 +21,65 @@ function Login(){
             setfilled(true);
         }
     },[username,password])
+
+
     return(
-        <View style={styles.form}>
-            <View style={styles.container}>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton onSuccess={filled}>Create</PrimaryButton>
+            <View style={styles.form}>
+                <View style={styles.bigdescription}>
+                    <Text style={styles.title}>Welcome!</Text>
+                    <Text style={styles.description}>Finding Carparks in a jiffy!</Text>
+                </View>
+                <View>
+                    <Input />
+                    <Input />
+                </View>
+                <View style={styles.container}>
+                    <View>
+                        <Text style={styles.text}>Don't have an account?</Text>
+                        <Pressable onPress={()=>{console.log('hello')}}>
+                            <Text style={styles.text}>Create account</Text>
+                        </Pressable>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onSuccess={filled}>Login</PrimaryButton>
+                    </View>
                 </View>
             </View>
-        </View>
     )
 }
 
 export default Login;
 
+const deviceHeight = Dimensions.get('window').height
+
 const styles = StyleSheet.create({
     form:{
+        backgroundColor: 'black',
         flex:1,
     },
+    bigdescription:{
+        marginTop: deviceHeight < 380 ? 60 : 170,
+        marginLeft: 25
+    },
+    title:{
+        fontSize: 30,
+        fontWeight: "bold",
+        color: 'white',
+    },
+    text:{
+        color: 'white'
+    },
+    description:{
+        color: 'white',
+        marginTop: 20,
+    },
     container: {
-        backgroundColor: 'black',
         flex: 1,
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        marginTop: deviceHeight < 380 ? 60 : 170,
+        marginHorizontal: 20
     },
     buttonContainer:{
-        marginTop: 300,
     }
 });
