e2bbaed39d97767475d6039b9c53d9a881c9840e
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-03-20 20:46:44 +0800

    Update Login.js

--- carpark/screens/Accounts/Login.js
+++ carpark/screens/Accounts/Login.js
@@ -62,7 +62,7 @@ function Login({navigation}){
                 <View style={styles.container}>
                     <View>
                         <Text style={styles.text}>Don't have an account?</Text>
-                        <Pressable onPress={()=>{navigation.navigate("ReportFault")}}>
+                        <Pressable onPress={()=>{navigation.navigate("Create Account")}}>
                             <Text style={styles.text}>Create account</Text>
                         </Pressable>
                     </View>
