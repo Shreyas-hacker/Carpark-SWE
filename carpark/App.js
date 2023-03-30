c73441d33263c5a8e629425d6902e286cff66eaf
LasVie <115763977+LasVie@users.noreply.github.com>
2023-03-30 12:47:11 +0800

    CarparkOnMaps module

--- carpark/App.js
+++ carpark/App.js
@@ -4,30 +4,34 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Login from "./screens/Accounts/Login";
 import CreateAccount from "./screens/Accounts/CreateAccount";
-import CreateProfile from './screens/Profile/CreateProfile'
+import CreateProfile from "./screens/Profile/CreateProfile";
 import Home from "./screens/DisplayCarparks/Home";
 import ForgetPassword from "./screens/Accounts/ForgetPassword";
 import ReportFault from "./screens/ReportFault";
-import Camera from './screens/Kamera';
-import DisplayCarpark from "./screens/DisplayCarparks/DCarpark";
+import Camera from "./screens/Kamera";
+import DisplayCarpark from "./screens/DisplayCarparks/DisplayCarparks";
 import { MyTabs } from "./screens/Tabs";
 import { StatusBar } from "expo-status-bar";
 import AuthContextProvider, { AuthContext } from "./store/context/user-context";
 
 const Stack = createNativeStackNavigator();
 
-function AuthStack(){
+function AuthStack() {
   return (
-    <Stack.Navigator screenOptions={{headerShown:false}}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="CreateAccount" component={CreateAccount} />
       <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
-      <Stack.Screen name="CreateProfile" component={CreateProfile} options={{ headerShown: false, gestureEnabled: false }}/>
+      <Stack.Screen
+        name="CreateProfile"
+        component={CreateProfile}
+        options={{ headerShown: false, gestureEnabled: false }}
+      />
     </Stack.Navigator>
-  )
+  );
 }
 
-function AuthenticatedStack(){
+function AuthenticatedStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen
@@ -46,25 +50,25 @@ function AuthenticatedStack(){
         }}
       />
     </Stack.Navigator>
-  )
+  );
 }
 
-function Navigation(){
+function Navigation() {
   const authCtx = useContext(AuthContext);
   return (
-      <NavigationContainer>
-        {!authCtx.isAuthenticated && <AuthStack />}
-        {authCtx.isAuthenticated &&<AuthenticatedStack/>}
-      </NavigationContainer>
-  )
+    <NavigationContainer>
+      {!authCtx.isAuthenticated && <AuthStack />}
+      {authCtx.isAuthenticated && <AuthenticatedStack />}
+    </NavigationContainer>
+  );
 }
 export default function App() {
   return (
     <>
-    <StatusBar style="light"/>
-    <AuthContextProvider>
-      <Navigation/>
-    </AuthContextProvider>
+      <StatusBar style="light" />
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
     </>
   );
 }
