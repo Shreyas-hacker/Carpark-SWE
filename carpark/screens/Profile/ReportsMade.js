90fd5e7bb7ad181ae78affee4d588c7298db05ad
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-04-06 06:34:54 +0800

    report made page starting

--- carpark/screens/Profile/ReportsMade.js
+++ carpark/screens/Profile/ReportsMade.js
@@ -1,14 +1,41 @@
-import { Text,StyleSheet } from "react-native";
+import { Text,StyleSheet,View,Dimensions } from "react-native";
+import { useState } from "react";
 
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import IconButton from "../../components/IconButton";
 
-function ReportsMade(){
+function ReportsMade({navigation}){
+    const [reportsDone, setReportsDone] = useState();
+
+    function goBack(){
+        navigation.goBack();
+    }
     return(
-        <Text>Reports Made!</Text>
+        reportsDone ? (<Text>Reports Made!</Text>):(
+            <>
+            <View style={styles.topContent}>
+                <IconButton onPress={goBack} icon="arrow-back" size={28} color="black"/>
+            </View>
+            <View style={styles.imageContainer}>
+                <MaterialIcons name="report-off" color="black" size={100} style={{alignSelf: 'center'}}/>
+                <Text style={{alignSelf:'center'}}>No reports made!</Text>
+            </View>
+            </>
+        )
     )
 }
 
-const styles = StyleSheet.create({
+const height  = Dimensions.get('window').height;
 
+const styles = StyleSheet.create({
+    topContent:{
+        flexDirection:'row',
+        marginTop: 40,
+        alignItems: 'center'
+    },
+    imageContainer:{
+        marginTop: height/3,
+    },
 })
 
 export default ReportsMade;
