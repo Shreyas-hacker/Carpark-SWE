8719310297cafe508dde145a234c5118a4b3d264
LasVie <115763977+LasVie@users.noreply.github.com>
2023-04-08 14:58:14 +0800

    Update CarparkInfoCard.js

--- carpark/screens/DisplayCarparks/CarparkInfoCard.js
+++ carpark/screens/DisplayCarparks/CarparkInfoCard.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 
 const CarparkInfoCard = ({ carpark }) => {
   return (
     <View style={styles.card}>
       <Text style={styles.title}>{carpark.address}</Text>
       <Text style={styles.subtitle}>
-        Lots available: {carpark.free_parking}
+        Free parking time: {carpark.free_parking}
       </Text>
+      <Text style={styles.subtitle}>Total Slots:</Text>
+      <Text style={styles.subtitle}>Avaliable Slots:</Text>
     </View>
   );
 };
@@ -19,7 +21,8 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     backgroundColor: "white",
-    padding: 16,
+    paddingTop: 16,
+    paddingBottom: Dimensions.get("window").height / 5,
     borderTopLeftRadius: 16,
     borderTopRightRadius: 16,
     shadowColor: "#000000",
@@ -32,13 +35,16 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   title: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 8,
+    marginBottom: 0,
+    marginLeft: 20,
   },
   subtitle: {
     fontSize: 14,
     color: "#888",
+    marginTop: 20,
+    marginLeft: 20,
   },
 });
 
