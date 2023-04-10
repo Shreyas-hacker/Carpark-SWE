2cedf5e16d26d4da08d57675e2da04d283884da7
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-04-10 15:47:02 +0800

    done with report cards shown on report made

---
+++ carpark/screens/Profile/ReportCard.js
@@ -0,0 +1,58 @@
+import { View,Text,StyleSheet } from "react-native";
+
+function ReportCard({report}){
+    const colors = ["#FFFFFF", "#FFC107", "#FF0000"];
+    const severity = ["Lighting", "Gantry Machine", "Barrier", "Others"];
+  
+    const backgroundColor = colors[report.severity-1];
+    const faultType = severity[report.fault];
+  
+    return (
+      <View style={styles.container}>
+        <View style={[styles.card, { backgroundColor }]}>
+          <Text style={styles.title}>{report.carpark}</Text>
+          <Text style={styles.text}>
+            Fault Type: <Text style={styles.boldText}>{faultType}</Text>
+          </Text>
+          <Text style={styles.text}>
+            Description:{" "}
+            <Text style={styles.boldText}>{report.description}</Text>
+          </Text>
+        </View>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 20,
+    },
+    card: {
+      backgroundColor: "#fff",
+      borderRadius: 10,
+      padding: 20,
+      elevation: 5,
+      maxWidth: 400,
+      width: "90%",
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 10,
+      textAlign: "center",
+      color: "#444444",
+    },
+    text: {
+      fontSize: 16,
+      marginBottom: 5,
+      color: "#444444",
+    },
+    boldText: {
+      fontWeight: "bold",
+    },
+  });
+
+export default ReportCard;
