59a1e390ab221f3840a70ae1e7673c04654629d3
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-04-12 05:59:14 +0800

    Update loadfont.js

--- carpark/util/fonts/loadfont.js
+++ carpark/util/fonts/loadfont.js
@@ -9,10 +9,6 @@ const useLoadFonts = () => {
         "OpenSans-Bold": require("../../assets/fonts/OpenSans-Bold.ttf"),
         "OpenSans-ExtraBold": require("../../assets/fonts/OpenSans-SemiBold.ttf"),
         "OpenSans-Medium": require("../../assets/fonts/OpenSans-Light.ttf"),
-        "OpenSans-Italic": require("../../assets/fonts/OpenSans-Italic.ttf"),
-        "OpenSans-BoldItalic": require("../../assets/fonts/OpenSans-BoldItalic.ttf"),
-        "OpenSans-ExtraBoldItalic": require("../../assets/fonts/OpenSans-ExtraBoldItalic.ttf"),
-        "OpenSans-MediumItalic": require("../../assets/fonts/OpenSans-MediumItalic.ttf"),
         "OpenSans_Condensed-Bold": require("../../assets/fonts/OpenSans_Condensed-Bold.ttf"),  
       });
     }
